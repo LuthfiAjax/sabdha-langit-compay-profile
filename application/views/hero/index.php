2fe77eff45090d4c4b90c486b73ce0a4c181705a
LuthfiAjax <a2l083d@gmail.com>
2023-04-30 00:30:58 +0700

    new

--- application/views/hero/index.php
+++ application/views/hero/index.php
@@ -72,7 +72,9 @@
             <div class="col-md-5">
                 <div class="titlepage">
                     <h2>Tentang Kami</h2>
-                    <p>Sabdha Langit Indonesia adalah sebuah perusahaan Event Organizer yang berbasis di Grobogan, Jawa Tengah. Perusahaan ini telah berpengalaman dalam mengelola berbagai jenis acara seperti expo, bazaar, konser musik, launching produk, dan banyak lagi. </p>
+                    <p>Sabdha Langit Indonesia adalah sebuah perusahaan Event Organizer yang berbasis di Grobogan, Jawa
+                        Tengah. Perusahaan ini telah berpengalaman dalam mengelola berbagai jenis acara seperti expo,
+                        bazaar, konser musik, launching produk, dan banyak lagi. </p>
                     <a class="read_more" href="Javascript:void(0)"> Selengkapnya</a>
                 </div>
             </div>
@@ -151,36 +153,39 @@
         </div>
         <div class="row">
             <style>
-                .h5 {
-                    display: -webkit-box;
-                    -webkit-box-orient: vertical;
-                    -webkit-line-clamp: 2;
-                    overflow: hidden;
-                    font-size: small;
-                }
+            .h5 {
+                display: -webkit-box;
+                -webkit-box-orient: vertical;
+                -webkit-line-clamp: 2;
+                overflow: hidden;
+                font-size: small;
+            }
 
-                .desc {
-                    display: -webkit-box;
-                    -webkit-box-orient: vertical;
-                    -webkit-line-clamp: 3;
-                    overflow: hidden;
-                    text-align: justify;
-                }
+            .desc {
+                display: -webkit-box;
+                -webkit-box-orient: vertical;
+                -webkit-line-clamp: 3;
+                overflow: hidden;
+                text-align: justify;
+            }
             </style>
             <?php foreach ($blogs as $row) : ?>
-                <div class="col-md-4">
-                    <div class="blog_box">
-                        <div class="blog_img">
-                            <a href="<?= base_url('blog/' . $row['header_image']); ?>">
-                                <figure><img src="<?= base_url('assets/images/articles/headers/' . $row['header_image']); ?>" alt="<?= $row['title_id']; ?>" /></figure>
-                            </a>
-                        </div>
-                        <div class="blog_room">
-                            <h3 class="h5 mb-2"><a href="<?= base_url('blog/' . $row['slug_id']); ?>"><?= $row['title_id']; ?></a></h3>
-                            <p class="desc"><?= $row['deskripsi']; ?></p>
-                        </div>
+            <div class="col-md-4">
+                <div class="blog_box">
+                    <div class="blog_img">
+                        <a href="<?= base_url('blog/' . $row['header_image']); ?>">
+                            <figure><img
+                                    src="<?= base_url('assets/images/articles/headers/' . $row['header_image']); ?>"
+                                    alt="<?= $row['title_id']; ?>" /></figure>
+                        </a>
+                    </div>
+                    <div class="blog_room">
+                        <h3 class="h5 mb-2"><a
+                                href="<?= base_url('blog/' . $row['slug_id']); ?>"><?= $row['title_id']; ?></a></h3>
+                        <p class="desc"><?= $row['deskripsi']; ?></p>
                     </div>
                 </div>
+            </div>
             <?php endforeach; ?>
         </div>
     </div>
@@ -221,7 +226,9 @@
             <div class="col-md-6">
                 <div class="map_main">
                     <div class="map-responsive">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.8652063315362!2d110.91876687515861!3d-7.025126292976601!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e70b748c90b7371%3A0x8047ad129c4a7f98!2sJl.%20Pangeran%20Puger%20No.70%2C%20Grobogan%2C%20Kec.%20Grobogan%2C%20Kabupaten%20Grobogan%2C%20Jawa%20Tengah%2058152!5e0!3m2!1sid!2sid!4v1680808315952!5m2!1sid!2sid" width="600" height="400" style="border:1;" allowfullscreen="" loading="lazy"></iframe>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.8652063315362!2d110.91876687515861!3d-7.025126292976601!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e70b748c90b7371%3A0x8047ad129c4a7f98!2sJl.%20Pangeran%20Puger%20No.70%2C%20Grobogan%2C%20Kec.%20Grobogan%2C%20Kabupaten%20Grobogan%2C%20Jawa%20Tengah%2058152!5e0!3m2!1sid!2sid!4v1680808315952!5m2!1sid!2sid"
+                            width="auto" height="400" style="border:1;" allowfullscreen="" loading="lazy"></iframe>
                     </div>
                 </div>
             </div>
--- application/views/hero/templates/header.php
+++ application/views/hero/templates/header.php
@@ -10,8 +10,10 @@
 
     <title>Sabdha Langit Indonesia</title>
 
-    <meta name="keywords" content="Event organizer, EO, Grobogan, Purwodadi, Jawa Tengah, BUMN, EO Pemerintah daerah, pemda, Semarang">
-    <meta name="description" content="Kami adalah perusahaan Event Organizer profesional di Grobogan, Jawa Tengah. Menawarkan layanan lengkap untuk acara pernikahan, acara bisnis, hingga Event Pemerintahan. Hubungi kami sekarang untuk mendapatkan solusi terbaik dan membuat acara Anda berkesan!">
+    <meta name="keywords"
+        content="Event organizer, EO, Grobogan, Purwodadi, Jawa Tengah, BUMN, EO Pemerintah daerah, pemda, Semarang">
+    <meta name="description"
+        content="Kami adalah perusahaan Event Organizer profesional di Grobogan, Jawa Tengah. Menawarkan layanan lengkap untuk acara pernikahan, acara bisnis, hingga Event Pemerintahan. Hubungi kami sekarang untuk mendapatkan solusi terbaik dan membuat acara Anda berkesan!">
     <meta name="author" content="jaxid">
 
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>css/bootstrap.min.css">
@@ -20,63 +22,62 @@
     <link rel="icon" href="<?= base_url('assets/'); ?>images/logo/faveicon.png" type="image/png" />
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>css/jquery.mCustomScrollbar.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
+        media="screen">
 
 </head>
 
 <body class="main-layout">
 
     <?php if ($pages == 'beranda') : ?>
-        <div class="loader_bg">
-            <div class="loader"><img src="<?= base_url('assets/'); ?>images/logo/logo-sabdha-langit.png" alt="Loading Images Sabdha Langit" /></div>
-        </div>
+    <div class="loader_bg">
+        <div class="loader"><img src="<?= base_url('assets/'); ?>images/logo/logo-sabdha-langit.png"
+                alt="Loading Images Sabdha Langit" /></div>
+    </div>
     <?php endif; ?>
 
     <header>
         <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
             <div class="container">
                 <a class="navbar-brand" href="#">
-                    <img src="<?= base_url('assets/'); ?>images/logo/logo-panjang-hitam.png" alt="Logo Sabdha Langit" width="200" class="d-inline-block align-text-top">
+                    <img src="<?= base_url('assets/'); ?>images/logo/logo-panjang-hitam.png" alt="Logo Sabdha Langit"
+                        width="200" class="d-inline-block align-text-top">
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <style>
+                    .active {
+                        border-bottom: 2px solid red;
+                        color: red;
+                    }
+
+                    .navbar-nav .nav-link {
+                        font-weight: 600;
+                        color: #000;
+                    }
+
+                    .navbar-nav .nav-link:hover {
+                        color: red;
+                    }
+                    </style>
                     <ul class="navbar-nav">
-                        <style>
-                            .active {
-                                border-bottom: 2px solid red;
-                                color: red;
-                            }
-                        </style>
-                        <style>
-                            .navbar-nav .nav-link {
-                                font-weight: 500;
-                                color: #000;
-                            }
-
-                            .navbar-nav .nav-link:hover {
-                                color: red;
-                            }
-                        </style>
-
                         <li class="nav-item <?= ($pages == 'beranda') ? 'active' : '' ?>">
-                            <a class="nav-link" href="<?= base_url(''); ?>">Beranda</a>
+                            <a class="nav-link mx-2" href="<?= base_url(''); ?>">Beranda</a>
                         </li>
                         <li class="nav-item <?= ($pages == '') ? 'active' : '' ?>">
-                            <a class="nav-link" href="#">Tentang Kami</a>
+                            <a class="nav-link mx-2" href="#">Tentang Kami</a>
                         </li>
                         <li class="nav-item <?= ($pages == '') ? 'active' : '' ?>">
-                            <a class="nav-link" href="#">Portofolio</a>
-                        </li>
-                        <li class="nav-item <?= ($pages == '') ? 'active' : '' ?>">
-                            <a class="nav-link" href="#">Layanan</a>
+                            <a class="nav-link mx-2" href="#">Layanan</a>
                         </li>
                         <li class="nav-item <?= ($pages == 'blog') ? 'active' : '' ?>">
-                            <a class="nav-link" href="<?= base_url('blog'); ?>">Blog</a>
+                            <a class="nav-link mx-2" href="<?= base_url('blog'); ?>">Blog</a>
                         </li>
                         <li class="nav-item <?= ($pages == '') ? 'active' : '' ?>">
-                            <a class="nav-link" href="#">Hubungi Kami</a>
+                            <a class="nav-link mx-2" href="#">Hubungi</a>
                         </li>
                     </ul>
                 </div>
